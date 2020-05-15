0135a884e568617aba9ab4b3c0fffe9bca01df6b
Snopoff <snopoffp@gmail.com>
2020-05-16 00:00:38 +0300

    finished it for situation when b0 != 0

--- Lab5/task5.py
+++ Lab5/task5.py
@@ -21,21 +21,24 @@ filename = 'data.txt'
 def get_data():
     """
     Get data method
-    Returns list of parameters 
+    Returns list of parameters
     """
     with open(filename, 'r') as f:
         lines = f.readlines()
     conds = list(map(lambda x: float(x), lines[0].split(" ")))
     b_cond1, b_cond2 = conds[:3], conds[3:]
     interval = list(map(lambda x: float(x), lines[1].split(" ")))
-    flat_X = [i for item in lines[2:] for i in item.rstrip().split(" ")]
+    flat_X = list(map(lambda x: int(
+        x) if x != '0.0' and float(x).is_integer() else float(x), [i for item in lines[2:] for i in item.rstrip().split(" ")]))
     X = od(dict(itertools.zip_longest(*[iter(flat_X)] * 2, fillvalue="")))
     return b_cond1, b_cond2, interval, X
 
 
-def write_data(X):
+def write_data(X, Y, Yprime):
     with open('rez.txt', 'w') as f:
-        f.write(str(X))
+        for key, value in X.items():
+            f.write(" ".join([str(key), str(value),
+                              str(Y[value]), str(Yprime[value]), "\n"]))
 
 
 def RungeKutta(f: Callable, h: np.double, x: np.double, y=0) -> np.double:
@@ -86,27 +89,62 @@ def forward(X: dict, b_cond1: list, interval: list):
     Z1 = od({a: -a0 / b0})
     Z2 = od({a: A / b0})
 
+    # Solve first given differential equation
     def func(x, z): return - z**2 - p(x)*z + q(x)
 
-    for i in range(1, int(n)):
-        x = next(reversed(Z1))
+    for i in range(0, len(X)-1):
+        # x = next(reversed(Z1))
+        x = X[i]
         z = Z1[x]
-        Z1[x + h] = RungeKutta(func, h, x, z)
+        Z1[X[i+1]] = RungeKutta(func, h, x, z)
 
-    def help_func(x): return Z1[x] + p(x)
-    def func(x, z): return - z * help_func(x) + f(x)
+    # solve second given differential equation
+    def func(z1): return lambda x, z: - z * z1 + p(x) + f(x)
 
-    for i in range(1, int(n)):
-        x = next(reversed(Z2))
+    for i in range(0, len(X)-1):
+        # x = next(reversed(Z2))
+        x = X[i]
         z = Z2[x]
-        print(z)
-        Z2[x + h] = RungeKutta(func, h, x, z)
-    return Z2
+        z1 = Z1[x]
+        Z2[X[i+1]] = RungeKutta(func(z1), h, x, z)
+    return Z1, Z2
+
+
+def backward(X: dict, b_cond2: list, interval: list, Z1: dict, Z2: dict):
+    """
+    Обратная прогонка: нужно решить следующую задачу Коши:
+        y' = z1 * y + z2
+        y(b) = (B - b1*z2(b)) / (a1 + b1*z1(b))
+    """
+
+    a1, b1, B = b_cond2
+    a, b, n = interval
+    h = (b - a) / n
+
+    Y = od({b: (B - b1*Z2[b]) / (a1 + b1*Z1[b])})
+
+    def func(z1, z2):
+        return lambda x, y: z1*y + z2
+
+    for i in range(len(X)-1, 0, -1):
+        # x = next(reversed(Z2))
+        x = X[i]
+        y = Y[x]
+        z1 = Z1[x]
+        z2 = Z2[x]
+        Y[X[i-1]] = RungeKutta(func(z1, z2), h, x, y)
+    return od(reversed(Y.items()))
 
 
 def solve():
     b_cond1, b_cond2, interval, X = get_data()
-    print(forward(X, b_cond1, interval))
+    Z1, Z2 = forward(X, b_cond1, interval)
+    Y = backward(X, b_cond2, interval, Z1, Z2)
+    Yprime = {}
+    for x in X.values():
+        Yprime[x] = Z1[x] * Y[x] + Z2[x]
+
+    write_data(X, Y, Yprime)
 
 
 def main():
